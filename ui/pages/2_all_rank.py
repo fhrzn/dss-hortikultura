4cadab11aedf1d51513e8edd5ba0128a858ad315
Affandy Fahrizain <fahri.lafa@gmail.com>
2024-04-19 09:05:58 +0700

    add: rank in all cities

---
+++ ui/pages/2_all_rank.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import logging
+from utils import dbutils, dssutils
+from dotenv import load_dotenv
+from controller import lahan, tanaman
+import json
+import pandas as pd
+import os
+
+
+logger = logging.getLogger(__name__)
+
+dbutils.init_db()
+
+
+st.set_page_config(
+    page_title="Hortikultura Ranks in All Cities",
+    page_icon="🌱",
+    initial_sidebar_state="collapsed",
+)
+
+st.write("# Rank in All Cities")
+
+
+# load data
+with open(os.getenv("EIGENVECTOR_JSON_PATH"), "r") as f:
+    eigenvectors = json.loads(f.read())
+
+lahans = lahan.read_lahan(db=st.session_state["db"], return_dict=False)
+tanamans = tanaman.read_tanaman(db=st.session_state["db"], return_dict=False)
+
+rank_kota = []
+
+for kota in lahans:
+    # add eigenvector attribute
+    kota["ev_jenis_tanah"] = eigenvectors["eigenvector"]["ahp_jenis_tanah"][kota["jenis_tanah"]]
+    kota["ev_tekstur_tanah"] = eigenvectors["eigenvector"]["ahp_tekstur_tanah"][kota["tekstur_tanah"]]
+    
+    tanaman_kota = tanamans.copy()
+    for t in tanaman_kota:
+        # add kota name
+        t["kota"] = kota["desa"]
+
+        # add eigenvector attribute
+        t["ev_jenis_tanah"] = eigenvectors["eigenvector"]["ahp_jenis_tanah"][t["jenis_tanah"].lower()]
+        t["ev_tekstur_tanah"] = eigenvectors["eigenvector"]["ahp_tekstur_tanah"][t["tekstur_tanah"].lower()]
+        
+        # compute gap value
+        t["gap_jenis_tanah"] = kota["ev_jenis_tanah"] - t["ev_jenis_tanah"]
+        t["gap_tekstur_tanah"] = kota["ev_tekstur_tanah"] - t["ev_tekstur_tanah"]
+
+        # compute bobot value
+        t["bobot_jenis_tanah"] = dssutils.interpolasi_gap(t["gap_jenis_tanah"])
+        t["bobot_tekstur_tanah"] = dssutils.interpolasi_gap(t["gap_tekstur_tanah"])
+
+        ###################
+        ### INTERPOLASI ###
+        ###################
+        # suhu
+        suhu_intp_range = list(map(int, t["suhu_interpolasi"].split(",")[1:]))
+        t["suhu_intp"] = dssutils.interpolate_4_points(kota["suhu"], *suhu_intp_range)
+
+        # curah hujan
+        curah_intp_range = list(map(int, t["curah_hujan_interpolasi"].split(",")[1:]))
+        t["curah_intp"] = dssutils.interpolate_4_points(kota["curah_hujan"], *curah_intp_range)
+        
+        # kelembapan
+        kelembapan_intp_range = list(map(int, t["kelembapan_interpolasi"].split(",")[1:]))
+        t["kelembapan_intp"] = dssutils.interpolate_4_points(kota["kelembapan"], *kelembapan_intp_range)
+        
+        # ph
+        ph_intp_range = list(map(float, t["ph_interpolasi"].split(",")[1:]))
+        t["ph_intp"] = dssutils.interpolate_4_points(kota["ph"], *ph_intp_range)
+        
+        # kemiringan
+        kemiringan_intp_range = list(map(int, t["kemiringan_interpolasi"].split(",")[1:]))
+        if len(kemiringan_intp_range) < 3:
+            t["kemiringan_intp"] = dssutils.interpolate_3_points(kota["kemiringan"], *kemiringan_intp_range)
+        else:
+            t["kemiringan_intp"] = dssutils.interpolate_4_points(kota["kemiringan"], *kemiringan_intp_range)
+        
+        # topografi
+        topografi_intp_range = list(map(int, t["topografi_interpolasi"].split(",")[1:]))
+        t["topografi_intp"] = dssutils.interpolate_4_points(kota["topografi"], *topografi_intp_range)
+        
+        # calculate nilai sub-kriteria iklim
+        t["suhu_nk"] = t["suhu_intp"] * eigenvectors["eigenvector"]["sub_kriteria_iklim"]["suhu"]
+        t["kelembapan_nk"] = t["kelembapan_intp"] * eigenvectors["eigenvector"]["sub_kriteria_iklim"]["kelembapan"]
+        t["curah_nk"] = t["curah_intp"] * eigenvectors["eigenvector"]["sub_kriteria_iklim"]["curah_hujan"]
+        t["iklim_nk"] = t["suhu_nk"] + t["kelembapan_nk"] + t["curah_nk"]
+
+        # calculate nilai sub-kriteria tanah
+        t["ph_nk"] = t["ph_intp"] * eigenvectors["eigenvector"]["sub_kriteria_tanah"]["ph"]
+        t["kemiringan_nk"] = t["kemiringan_intp"] * eigenvectors["eigenvector"]["sub_kriteria_tanah"]["kemiringan"]
+        t["jenis_tanah_nk"] = t["bobot_jenis_tanah"] * eigenvectors["eigenvector"]["sub_kriteria_tanah"]["jenis_tanah"]
+        t["tekstur_tanah_nk"] = t["bobot_tekstur_tanah"] * eigenvectors["eigenvector"]["sub_kriteria_tanah"]["tekstur_tanah"]
+        t["tanah_nk"] = t["ph_nk"] + t["kemiringan_nk"] + t["jenis_tanah_nk"] + t["tekstur_tanah_nk"]
+
+        # calculate profile matching
+        t["pm_score"] = t["iklim_nk"] * eigenvectors["eigenvector"]["kriteria"]["iklim"] + \
+                        t["tanah_nk"] * eigenvectors["eigenvector"]["kriteria"]["tanah"] + \
+                        t["topografi_intp"] * eigenvectors["eigenvector"]["kriteria"]["topografi"]
+
+    # ranking    
+    df_rank = pd.DataFrame.from_dict(tanaman_kota)
+    cols = ["kota", "jenis_tanaman", "pm_score"]
+    df_rank = df_rank[cols].sort_values("pm_score", ascending=False, ignore_index=True)
+    df_rank['rank'] = [i for i in range(1, len(df_rank)+1)]
+
+    rank_kota.append(df_rank)
+
+df_all = pd.concat(rank_kota, axis=0, ignore_index=True)
+st.dataframe(df_all, use_container_width=True, height=1000)
