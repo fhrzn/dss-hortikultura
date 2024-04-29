38e56d1cf70b15942243a1b33d5a0c505625b86a
Affandy Fahrizain <fahri.lafa@gmail.com>
2024-04-30 06:26:03 +0700

    finale (#9)

--- ui/pages/3_evaluation.py
+++ ui/pages/3_evaluation.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import logging
 from utils import dbutils, dssutils
-from dotenv import load_dotenv
 from controller import lahan, tanaman
 import json
 import pandas as pd
 import os
+from sklearn.metrics import classification_report, accuracy_score, f1_score, recall_score, precision_score, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt 
+import seaborn as sns
 
 
 logger = logging.getLogger(__name__)
@@ -14,12 +16,12 @@ dbutils.init_db()
 
 
 st.set_page_config(
-    page_title="Hortikultura Ranks in All Cities",
+    page_title="Hortikultura Evaluasi per Kota",
     page_icon="🌱",
     initial_sidebar_state="collapsed",
 )
 
-st.write("# Rank in All Cities")
+st.write("# Evaluasi di tiap Kota")
 
 
 # load data
@@ -106,7 +108,7 @@ for kota in lahans:
     cols = ["kota", "jenis_tanaman", "pm_score"]
     df_rank = df_rank[cols].sort_values("pm_score", ascending=False, ignore_index=True)
     df_rank['rank'] = [i for i in range(1, len(df_rank)+1)]
-    df_rank = df_rank[df_rank['rank'] <= 3]
+    # df_rank = df_rank[df_rank['rank'] <= 3]
     df_rank = df_rank[['kota', 'jenis_tanaman', 'rank']]
 
     rank_kota.append(df_rank)
@@ -115,4 +117,55 @@ df_all = pd.concat(rank_kota, axis=0, ignore_index=True)
 df_all = df_all.pivot(index="kota", columns="rank", values="jenis_tanaman").add_prefix("Rank ").reset_index().rename_axis(None, axis=1)
 # df_all['predicted'] = df_all['Rank 1']
 # df_all = pd.pivot_table(df_all, index="kota", columns="jenis_tanaman")
-st.dataframe(df_all, use_container_width=True, height=1000)
+# st.dataframe(df_all, use_container_width=True, height=1000)
+
+# display evaluation on each city
+df_grt = pd.read_csv("./data/ground_truth.csv", sep=';')
+
+# data pairing
+pair_grt_pred = {}
+for _, data in df_all.iterrows():
+    d = data.to_list()
+    pair_grt_pred[d[0]] = {}
+    pair_grt_pred[d[0]]["predicted"] = [i.lower() for i in d[1:]]
+    pair_grt_pred[d[0]]["actual"] = df_grt[df_grt["kota"] == d[0]]["tanaman"].item().split(", ")
+    # st.write(d[0])
+
+# evaluation
+l2i = {}
+i2l = {}
+cities = list(pair_grt_pred.keys())
+
+eval_city = st.selectbox("Pilih Kota", cities, index=None)
+
+if eval_city:
+    data = pair_grt_pred[eval_city]
+    if not l2i:
+        labels = sorted(data["predicted"])
+        l2i = {v: k for k, v in enumerate(labels)}
+        i2l = {k: v for k, v in enumerate(labels)}
+
+    y_true = [l2i[i] for i in data["actual"]][:3]
+    y_pred = [l2i[i] for i in data["predicted"] if l2i[i] in y_true]
+    y_label = [i2l[i] for i in y_true]
+
+    st.write(f"Accuracy Score: {accuracy_score(y_true, y_pred) * 100:.2f}%")
+    st.write(f"Recall Score: {recall_score(y_true, y_pred, average='weighted') * 100:.2f}%")
+    st.write(f"Precision Score: {precision_score(y_true, y_pred, average='weighted') * 100:.2f}%")
+    
+
+    cm = confusion_matrix(y_true, y_pred)
+    
+    fig, ax = plt.subplots(1, 1, figsize=(8,6))
+    ax = sns.heatmap(cm, annot=True, fmt='d')
+    ax.xaxis.set_ticklabels(y_label)
+    ax.yaxis.set_ticklabels(y_label)
+    ax.set_xlabel("\nPredicted Label")
+    ax.set_ylabel("True Label\n")
+    ax.set_title("\nConfusion Matrix\n")
+
+    st.pyplot(fig)
+
+
+    
+
