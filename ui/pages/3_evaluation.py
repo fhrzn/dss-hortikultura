190a8135f7548a04180bbd90c3b03cf0e90a5710
Affandy Fahrizain <fahri.lafa@gmail.com>
2024-08-10 06:31:14 +0700

    hotfix: update algos in eval page

--- ui/pages/3_evaluation.py
+++ ui/pages/3_evaluation.py
@@ -21,8 +21,10 @@ dbutils.init_db()
 def calculate_metrics(data, ntop: int = None):
     def _calculate_top_metrics(data, ntop):
         try:
+            if len(data['actual']) < ntop:
+                ntop = len(data['actual'])
             top_pred = data['predicted'][:ntop]
-            top_actual = data['actual']
+            top_actual = data['actual'][:ntop]
             intersect = set(top_pred).intersection(set(top_actual))
             return 1 if intersect else 0
         except:
